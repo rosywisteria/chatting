35978f7d4d30c0f32594ea038719570823efe1ef
emily wei <141193425+rosywisteria@users.noreply.github.com>
2025-09-03 19:56:35 -0400

    -changed frontend url

--- back/server.js
+++ back/server.js
@@ -27,7 +27,8 @@ run().catch(console.dir);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", 
+    // origin: "http://localhost:3000", 
+    origin: "https://chatting-3tub.onrender.com", 
     methods: ["GET", "POST"]
   }
 });
