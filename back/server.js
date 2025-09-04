b20780b558cc20df18c35bfd4bd2eaeb200de9ef
emily wei <141193425+rosywisteria@users.noreply.github.com>
2025-09-04 00:05:05 -0400

    -back

--- back/server.js
+++ back/server.js
@@ -31,13 +31,13 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000", 
-    origin: ["http://localhost:3000", "https://chat-deployment-w81d.onrender.com"],
+    origin: ["http://localhost:3000", "https://chatting-3tub.onrender.com"],
     methods: ["GET", "POST"]
   }
 });
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://chat-deployment-w81d.onrender.com"],
+  origin: ["http://localhost:3000", "https://chatting-3tub.onrender.com"],
   methods: ["GET","POST"],
   credentials: true
 }));
